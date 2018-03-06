4cd1693f5f159106b1a35df3c35689ee306a88a7
olivierag <olivier.agier@gmail.com>
2018-03-06 16:59:04 +0100

    Revert "Merge branch 'mathieu' into olivier"
    This reverts commit 970dfd9b13e89b180fca12a4dbddacbfe00d854e, reversing
    changes made to 3756b0a045892d24d8a1badc8f36dc453c9113ec.

--- core/src/com/mygdx/game/Squarz.java
+++ core/src/com/mygdx/game/Squarz.java
@@ -4,19 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-<<<<<<< HEAD
 import com.mygdx.game.control.GameStateManager;
 import com.mygdx.game.view.Menu;
-=======
-import com.mygdx.game.State.AIPreferencesState;
-import com.mygdx.game.State.GameStateManager;
-import com.mygdx.game.State.MenuState;
->>>>>>> mathieu
 
 public class Squarz extends ApplicationAdapter {
 	public final String TITLE = "Squarz";
-	public final static int WIDTH = 720;
-	public final static int HEIGHT = 1240;
 
 	private GameStateManager gsm;
 	private SpriteBatch batch;
@@ -26,19 +18,15 @@ public class Squarz extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
-<<<<<<< HEAD
 		Gdx.gl.glClearColor((float)0.84,(float)0.84,(float)0.84, 1);
 
 		gsm.push(new Menu(gsm));
-=======
-		gsm.push(new AIPreferencesState(gsm));
->>>>>>> mathieu
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Gdx.gl.glClearColor(100, 0, 50, 1);
+
 		gsm.updtate(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
 	}
@@ -46,5 +34,5 @@ public class Squarz extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-	}
+	} // Hello
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,6 @@ import com.mygdx.game.Squarz;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = Squarz.WIDTH;
-		config.height = Squarz.HEIGHT;
 		new LwjglApplication(new Squarz(), config);
 	}
 }
