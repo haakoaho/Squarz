d548fb8c518aa5fc88ca6cd963c7725dbce86570
emnzer <mathieu.blaisedugas@gmail.com>
2018-03-06 16:49:15 +0100

    16:45

--- core/src/com/mygdx/game/Squarz.java
+++ core/src/com/mygdx/game/Squarz.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.State.AIPreferencesState;
 import com.mygdx.game.State.GameStateManager;
 import com.mygdx.game.State.MenuState;
 
 public class Squarz extends ApplicationAdapter {
 	public final String TITLE = "Squarz";
+	public final static int WIDTH = 720;
+	public final static int HEIGHT = 1240;
 
 	private GameStateManager gsm;
 	private SpriteBatch batch;
@@ -18,15 +21,13 @@ public class Squarz extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-
-		gsm.push(new MenuState(gsm));
+		gsm.push(new AIPreferencesState(gsm));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+		Gdx.gl.glClearColor(100, 0, 50, 1);
 		gsm.updtate(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
 	}
@@ -34,5 +35,5 @@ public class Squarz extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-	} // Hello
+	}
 }
