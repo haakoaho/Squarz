87da11a3d921da6e1583fe12e7163653b46c03f2
haakoaho <haakoaho@stud.ntnu.no>
2018-03-06 14:20:23 +0100

    skeleton

---
+++ core/src/com/mygdx/game/Squarz.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.State.GameStateManager;
+import com.mygdx.game.State.MenuState;
+
+public class Squarz extends ApplicationAdapter {
+	public final String TITLE = "Squarz";
+
+	private GameStateManager gsm;
+	private SpriteBatch batch;
+
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+
+		gsm.push(new MenuState(gsm));
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		gsm.updtate(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
---
+++ core/src/com/mygdx/game/State/GameStateManager.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.State;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+public class GameStateManager {
+    private Stack<State> states;
+
+    public GameStateManager(){
+        states = new Stack<State>();
+    }
+
+    public void push(State state){
+        states.push(state);
+    }
+
+    public void pop(){
+        states.pop();
+    }
+
+    public void set(State state){
+        states.pop();
+        states.push(state);
+    }
+
+    public void updtate(float dt){
+        states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb){
+        states.peek().render(sb);
+    }
+}
