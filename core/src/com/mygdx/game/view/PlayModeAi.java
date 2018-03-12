e64b894982c9f2212c16366f8a278f56beff3a64
LucasLotterie <32510445+LucasLotterie@users.noreply.github.com>
2018-03-12 10:23:51 +0100

    10:23

--- core/src/com/mygdx/game/view/PlayModeAi.java
+++ core/src/com/mygdx/game/view/PlayModeAi.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.control.GameStateManager;
 import com.mygdx.game.model.Square;
@@ -19,7 +20,7 @@ import static com.mygdx.game.Squarz.WIDTH;
  * Created by Antoine Dc on 06/03/2018.
  */
 
-public class PlayModeAi extends State {
+public class PlayModeAi extends State implements GestureDetector.GestureListener {
     private Texture background;
     private Square choiceSquare;
     private Map<Integer, Square> leftMap;
@@ -153,5 +154,50 @@ public class PlayModeAi extends State {
                 rightMap.get(i).getTexture().dispose();
             }
         }
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
     }
 }
