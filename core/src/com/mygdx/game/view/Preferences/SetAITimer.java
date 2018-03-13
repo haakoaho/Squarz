9a3da0b457daaa57bd5a2497062a89aa2ce219f4
LucasLotterie <32510445+LucasLotterie@users.noreply.github.com>
2018-03-13 17:23:54 +0100

    17:23

--- core/src/com/mygdx/game/view/Preferences/SetAITimer.java
+++ core/src/com/mygdx/game/view/Preferences/SetAITimer.java
@@ -6,10 +6,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.Squarz;
 import com.mygdx.game.control.GameStateManager;
 import com.mygdx.game.control.aI.PreferencesSettings;
+import com.mygdx.game.model.Icon;
 import com.mygdx.game.model.State;
 import com.mygdx.game.view.AIPreferences;
 
 import java.util.WeakHashMap;
+
+import static com.mygdx.game.Squarz.HEIGHT;
+import static com.mygdx.game.Squarz.WIDTH;
 
 /**
  * Created by mathi on 12/03/2018.
@@ -17,32 +21,41 @@ import java.util.WeakHashMap;
 
 public class SetAITimer extends State {
     private PreferencesSettings set;
-    private Texture add, delete, timer;
+    private Icon add, delete, timer;
 
 
     public SetAITimer(GameStateManager gsm, PreferencesSettings setting){
         super(gsm);
-        this.set = setting;
-        this.add = new Texture(Gdx.files.internal("add.png"));
-        this.delete = new Texture(Gdx.files.internal("delete.png"));
-        this.timer = new Texture(Gdx.files.internal("ai_settings/setTimer.png"));
+        add = new Icon(new Texture(Gdx.files.internal("add.png")),0,0);
+        delete = new Icon(new Texture(Gdx.files.internal("delete.png")),0,0);
+        timer = new Icon(new Texture(Gdx.files.internal("ai_settings/setTimer.png")),0,0);
+        set = setting;
+
+        add.setPosx(WIDTH/2-add.getTexture().getWidth()/2);
+        add.setPosy(HEIGHT*2/3-add.getTexture().getHeight()/2);
+        delete.setPosx(WIDTH/2-add.getTexture().getWidth()/2);
+        delete.setPosy(HEIGHT/3-add.getTexture().getHeight()/2);
+        timer.setPosx(WIDTH/2-timer.getTexture().getWidth()/2);
+        timer.setPosy(HEIGHT/2 - timer.getTexture().getHeight()/2);
     }
 
 
     @Override
     public void handleInput() {
         if(Gdx.input.justTouched()){
-            if(Gdx.input.getY()<Gdx.graphics.getHeight()/3+add.getHeight()/2) { //add
+            int x = Gdx.input.getX();
+            int y = HEIGHT - Gdx.input.getY();
+            if(add.isIn(x,y)) { //add
                 if (this.set.getTimer().getDuration() != 60) {
                     this.set.getTimer().increment();
                 }
             }
-            if(Gdx.input.getY()>Gdx.graphics.getHeight()*2/3-delete.getHeight()/2) { //delete
+            if(delete.isIn(x,y)) { //delete
                 if (this.set.getTimer().getDuration() != 30) {
                     this.set.getTimer().decrement();
                 }
             }
-            if(Gdx.input.getY()>Gdx.graphics.getHeight()/3+add.getHeight()/2 && Gdx.input.getY()<Gdx.graphics.getHeight()*2/3-delete.getHeight()/2){
+            if(timer.isIn(x,y)){
                 gsm.set(new AIPreferences(gsm, set));
             }
         }
@@ -57,21 +70,21 @@ public class SetAITimer extends State {
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(add, Gdx.graphics.getWidth()/2-add.getWidth()/2, Gdx.graphics.getHeight()*2/3-add.getHeight()/2);
-        sb.draw(delete, Gdx.graphics.getWidth()/2-add.getWidth()/2, Gdx.graphics.getHeight()/3-add.getHeight()/2);
+
+        sb.draw(add.getTexture(), add.getPosx(), add.getPosy());
+        sb.draw(delete.getTexture(),delete.getPosx(), delete.getPosy());
 
         if(this.set.getTimer().getDuration() == 30){
-            sb.draw(timer, Gdx.graphics.getWidth()/2-timer.getWidth()/2, Gdx.graphics.getHeight()/2 - timer.getHeight()/2);
+            sb.draw(timer.getTexture(),timer.getPosx() ,timer.getPosy());
         }
         if(this.set.getTimer().getDuration() == 45){
-            sb.draw(timer, Gdx.graphics.getWidth()*2/5-timer.getWidth()/2-10, Gdx.graphics.getHeight()/2 - timer.getHeight()/2);
-            sb.draw(timer, Gdx.graphics.getWidth()*3/5-timer.getWidth()/2+10, Gdx.graphics.getHeight()/2 - timer.getHeight()/2);
-
+            sb.draw(timer.getTexture(),timer.getPosx()-timer.getTexture().getWidth()/2 -5 ,timer.getPosy());
+            sb.draw(timer.getTexture(),timer.getPosx()+timer.getTexture().getWidth()/2 +5,timer.getPosy());
         }
         if(this.set.getTimer().getDuration() == 60){
-            sb.draw(timer, Gdx.graphics.getWidth()/3-timer.getWidth()/2-10, Gdx.graphics.getHeight()/2 - timer.getHeight()/2);
-            sb.draw(timer, Gdx.graphics.getWidth()/2-timer.getWidth()/2, Gdx.graphics.getHeight()/2 - timer.getHeight()/2);
-            sb.draw(timer, Gdx.graphics.getWidth()*2/3-timer.getWidth()/2+10, Gdx.graphics.getHeight()/2 - timer.getHeight()/2);
+            sb.draw(timer.getTexture(),timer.getPosx()-timer.getTexture().getWidth()-10 ,timer.getPosy());
+            sb.draw(timer.getTexture(),timer.getPosx() ,timer.getPosy());
+            sb.draw(timer.getTexture(),timer.getPosx()+timer.getTexture().getWidth()+10 ,timer.getPosy());
         }
 
 
